d525f9959014d60f59a1afcadf5a1bf4d5080e03
Chinmay Borade <chinmayycodes@gmail.com>
2024-10-26 14:39:16 +0530

    hide icons git

--- app.py
+++ app.py
@@ -44,6 +44,12 @@ if 'todo_list' not in st.session_state:
 # Custom CSS for animations and layout
 st.markdown("""
 <style>
+.css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+    .viewerBadge_text__1JaDK {
+        display: none;
+    }
+    
 @keyframes fadeIn {
     0% {opacity: 0;}
     100% {opacity: 1;}
@@ -83,14 +89,6 @@ st.markdown(
 )
 
 
-
-# Add custom CSS to hide the GitHub icon
-hide_github_icon = """
-#GithubIcon {
-  visibility: hidden;
-}
-"""
-st.markdown(hide_github_icon, unsafe_allow_html=True)
 
 # Function to convert PDF to images and create a zip file
 def convert_pdf_to_images_zip(pdf_path):
